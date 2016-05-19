e9f13eefb8b54ab5ec34bc859a2c4a3bd8847fed
tythompson <tythompson@gmail.com>
2016-05-18 20:29:03 -0400

    Added methods

--- hangman.rb
+++ hangman.rb
@@ -1,56 +1,85 @@
-# dict_words = File.read("words.txt").downcase.split("\n") #compatibility with Windows and Mac
-dict_words = ["test"]
-word = dict_words.sample
-word_array = word.downcase.split("")
-blank_array = Array.new(word.length, "_")
-gameover = false
-guesses = 6
-input = ""
-guessed = []
-letter_check = 0
-letter_pos = nil
-until gameover do
+def questions(blank_array, guessed, guesses)
   puts
   print "Hangman: ", blank_array.join(" ")
   puts
-  print "Guessed letters: ", guessed.join(" ")
+  print "Wrong letters: ", guessed.join(" ")
   puts
   print "Guesses left: #{guesses}"
-  guesses -= 1
   puts
   puts "Guess a letter: "
-  input = gets.chomp.downcase
-  if guessed.include?(input)
-    puts "Please type a new letter"
-    guesses += 1
-  elsif word_array.include?(input)
+end
 
-    guesses += 1
-    puts "That letter was found!"
+def get_input
+  input = gets.chomp.downcase
+  return input
+end
+
+def guess(input, guessed, guesses, word_array, blank_array)
+  guesses -=1	
+  if guessed.include?(input)
+    puts "You already guessed #{input}. Please type a new letter"
+    return false
+  elsif word_array.include?(input)
+    puts "That letter was found in the hidden word!"
     count = 0
     word_array.each do |letter|
       if input == letter
-        blank_array.delete_at(count)
-        blank_array.insert(count, input)
+		blank_array[count] = input
       end
       count += 1
     end
+    return false
+  else
+     puts "That letter is not in the hidden word!"
+	 guessed << input
+	 return true
   end
-  #victory condition
-  if word_array == blank_array  || input == word
-    gameover = true
-    puts
-    print "You won! The word was: #{word}"
-    puts
-    break
-  end
-  #losing condition
-  if guesses == 0
-    gameover = true
-    puts
-    print "You lost! The word was: #{word}"
-    puts
-    break
-  end
-  guessed << input
 end
+
+#victory condition
+def win(word)
+	puts
+	print "You won! The word was: #{word}"
+	puts
+	gameover = true
+	return gameover
+end
+
+#losing condition
+def lose(word)
+	puts
+	print "You lost! The word was: #{word}"
+	puts
+	gameover = true
+	return gameover
+end
+
+def play(w, number_of_guesses)
+	word = w
+	blank_array = Array.new(word.length, "_")
+	guessed = []
+	guesses = number_of_guesses
+	word_array = word.downcase.split("")
+	gameover = false
+
+	until gameover == true
+		questions(blank_array, guessed, guesses)
+
+		input = get_input
+
+		if guess(input, guessed, guesses, word_array, blank_array)
+			guesses -=1
+		end
+
+		if word_array == blank_array  || input == word
+			gameover = win(word) 
+		end
+
+		if guesses == 0
+			gameover = lose(word) 
+		end
+	end
+	puts "End of game" if gameover
+end
+
+play(File.read("words.txt").downcase.split("\n").sample, 6)
