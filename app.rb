1d32cade56e54bfb55cbdea73f18931d87f5e1a7
tythompson <tythompson@gmail.com>
2016-06-07 00:39:03 -0400

    class instance saves

--- app.rb
+++ app.rb
@@ -1,113 +1,14 @@
-class Hangman
-	def initialize
-	# Game.new
-	play(File.read("words.txt").downcase.split("\n").sample, 6)
-	end
+require "pry"
+require "./db/setup"
+require "./lib/all"
 
-	def get_name
-		name = gets.chomp.downcase
-		# Game.name = name
-		# Game.save
-	 	return name
-	end
+def get_name
+  puts "What is your name?"
+  name = gets.chomp.capitalize
+  return name
+end
 
-	def questions(blank_array, guessed, guesses)
-	  puts
-	  print "Hangman: ", blank_array.join(" ")
-	  puts
-	  print "Wrong letters: ", guessed.join(" ")
-	  puts
-	  print "Guesses left: #{guesses}"
-	  puts
-	  puts "Guess a letter: "
-	end
+hang = Game.new(get_name)
 
-	def get_input
-	  input = gets.chomp.downcase
-	  return input
-	end
-
-	def guess(input, guessed, guesses, word_array, blank_array)
-	  guesses -=1
-	  if guessed.include?(input)
-	    puts "You already guessed #{input}. Please type a new letter"
-	    return false
-	  elsif word_array.include?(input)
-	    puts "That letter was found in the hidden word!"
-	    count = 0
-	    word_array.each do |letter|
-	      if input == letter
-	      	# Game.guessed_letters << input
-	      	# Game.save
-			    blank_array[count] = input
-	      end
-	    count += 1
-	    end
-	    return false
-	  else
-	    puts "That letter is not in the hidden word!"
-		  guessed << input
-		  # Game.guessed_letters << input
-		  # Game.save
-		  return true
-	  end
-	end
-
-	#victory condition
-	def win(word)
-		puts
-		print "You won! The word was: #{word}"
-		puts
-		gameover = true
-		# Game.games_won += 1
-		# Game.save
-		return gameover
-	end
-
-	#losing condition
-	def lose(word)
-		puts
-		print "You lost! The word was: #{word}"
-		puts
-		gameover = true
-		return gameover
-	end
-
-	def play(w, number_of_guesses)
-		word = w
-		# Game.word = w
-		# Game.save
-		blank_array = Array.new(word.length, "_")
-		guessed = []
-		guesses = number_of_guesses
-		word_array = word.downcase.split("")
-		gameover = false
-
-
-	  get_name
-		until gameover == true
-			questions(blank_array, guessed, guesses)
-
-			input = get_input
-
-			if guess(input, guessed, guesses, word_array, blank_array)
-				guesses -=1
-			end
-
-			if word_array == blank_array  || input == word
-				gameover = win(word)
-			end
-
-			if guesses == 0
-				gameover = lose(word)
-			end
-		end
-		puts "End of game"
-		# Game.games_played += 1
-		# Game.save
-		# puts "Games Won:", Game.games_won
-		# puts "Games Played:", Game.games_played
-  end
-end #ends Hangman class
-
-hang = Hangman.new
+hang.play(6)
+hang.play(6)
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,96 @@
 class Game < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  attr_accessor :games_played
+
+  def initialize(name)
+    @name = name
+    @games_won = 0
+    @games_played = 0
+    @guessed_letters = []
+    @word = ""
+  end
+
+  def questions(blank_array, guessed_letters, guesses)
+	  puts "Hangman: ", blank_array.join(" ")
+	  puts "Wrong letters: ", guessed_letters.join(" ")
+	  puts "Guesses left: #{guesses}"
+	  puts
+	  puts "Guess a letter: "
+	end
+
+  def get_input
+    input = gets.chomp.downcase
+	  return input
+  end
+
+  def guess(input, guessed_letters, guesses, word_array, blank_array)
+    guesses -=1
+    if guessed_letters.include?(input)
+      puts "You already guessed #{input}. Please type a new letter"
+      return false
+    elsif word_array.include?(input)
+      puts "That letter was found in the hidden word!"
+      count = 0
+      word_array.each do |letter|
+        if input == letter
+          blank_array[count] = input
+        end
+      count += 1
+      end
+      return false
+    else
+      puts "That letter is not in the hidden word!"
+      guessed_letters << input
+      return true
+    end
+  end
+
+  #victory condition
+  def win(word)
+    puts "You won! The word was: #{word}"
+    puts
+    gameover = true
+    @games_won += 1
+    return gameover
+  end
+
+  #losing condition
+  def lose(word)
+    puts "You lost! The word was: #{word}"
+    puts
+    gameover = true
+    return gameover
+  end
+
+  def play(number_of_guesses)
+    @word = File.read("words.txt").downcase.split("\n").sample
+    blank_array = Array.new(@word.length, "_")
+    guesses = number_of_guesses
+    word_array = @word.downcase.split("")
+    gameover = false
+
+    until gameover == true
+      questions(blank_array, @guessed_letters, guesses)
+
+      input = get_input
+
+      if guess(input, @guessed_letters, guesses, word_array, blank_array)
+        guesses -=1
+      end
+
+      if word_array == blank_array  || input == @word
+        gameover = win(@word)
+      end
+
+      if guesses == 0
+        gameover = lose(@word)
+      end
+    end
+    puts "End of game"
+    @games_played += 1
+    @guessed_letters = []
+
+    puts "Games Won:", @games_won
+    puts "Games Played:", @games_played
+  end
 end
